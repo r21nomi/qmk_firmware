0f3aabdb61b11575415634f7f1c187a2fb2909f1
r21nomi <ryotakosu@gmail.com>
2018-06-20 21:25:29 +0900

    Modiy keymap

--- keyboards/ergodox_ez/keymaps/r21nomi/keymap.c
+++ keyboards/ergodox_ez/keymaps/r21nomi/keymap.c
@@ -71,13 +71,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,---------------------------------------------------.           ,--------------------------------------------------.
  * |Version  |  F1  |  F2  |  F3  |  F4  |  F5  |      |           |      |  F6  |  F7  |  F8  |  F9  |  F10 |   F11  |
  * |---------+------+------+------+------+------+------|           |------+------+------+------+------+------+--------|
- * |    '    |   !  |   @  |   {  |   }  |   |  |      |           |      |   Up |   7  |   8  |   9  |   +  |   F12  |
+ * |    '    |   !  |   @  |   {  |   }  |   |  |      |           |      |      |   7  |   8  |   9  |   +  |   F12  |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
- * |    &    |   #  |   $  |   (  |   )  |   `  |------|           |------| Down |   4  |   5  |   6  |   -  |   =    |
+ * |    &    |   #  |   $  |   (  |   )  |   `  |------|           |------|      |   4  |   5  |   6  |   -  |   =    |
  * |---------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |         |   %  |   ^  |   [  |   ]  |   ~  |      |           |      |   0  |   1  |   2  |   3  |   *  |        |
  * `---------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | EPRM  |      |      |      |      |                                       |      |    . |   0  |   /  |      |
+ *   | EPRM  |      |      |      |      |                                       |   0  |    . |      |   /  |      |
  *   `-----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |Animat|      |       |Toggle|Solid |
@@ -99,11 +99,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                KC_TRNS,
                                KC_DEL, RGB_VAI,KC_TRNS,
        // right hand
-       KC_TRNS, KC_F6,   KC_F7,  KC_F8,   KC_F9,   KC_F10,  KC_F11,
-       KC_TRNS, KC_UP,   KC_7,   KC_8,    KC_9,    KC_PLUS, KC_F12,
-                KC_DOWN, KC_4,   KC_5,    KC_6,    KC_MINS, KC_EQL,
-       KC_TRNS, KC_0,    KC_1,   KC_2,    KC_3,    KC_ASTR, KC_TRNS,
-                         KC_TRNS,KC_DOT,  KC_TRNS, KC_SLSH, KC_TRNS,
+       KC_TRNS, KC_F6,   KC_F7, KC_F8,   KC_F9,   KC_F10,  KC_F11,
+       KC_TRNS, KC_TRNS, KC_7,  KC_8,    KC_9,    KC_PLUS, KC_F12,
+                KC_TRNS, KC_4,  KC_5,    KC_6,    KC_MINS, KC_EQL,
+       KC_TRNS, KC_0,    KC_1,  KC_2,    KC_3,    KC_ASTR, KC_TRNS,
+                         KC_0,  KC_DOT,  KC_TRNS, KC_SLSH, KC_TRNS,
        RGB_TOG, RGB_SLD,
        KC_TRNS,
        KC_TRNS, RGB_HUD, KC_BSPC
@@ -111,34 +111,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap 2: Navigation Layer
  *
  * ,----------------------------------------------------.           ,--------------------------------------------------.
- * | RESET|      |        |      |        |      |      |           |      |      |      |      |      |      |        |
+ * | RESET|      |        | PgUp |        |      |      |           |      |      |      |      |      |      |        |
  * |------+------+--------+------+--------+-------------|           |------+------+------+------+------+------+--------|
  * |      |      |Prev tab|   ↑  |Next tab|      |      |           |      |      |      | MsUp |      |      |        |
  * |------+------+--------+------+--------+------|      |           |      |------+------+------+------+------+--------|
  * |      | Home |    ←   |   ↓  |   →    |  End |------|           |------|      |MsLeft|MsDown|MsRght|      |  Play  |
  * |------+------+--------+------+--------+------|      |           |      |------+------+------+------+------+--------|
- * |      |      |        |      |        |      |      |           |      |      |      | Prev | Next |      |        |
+ * |      |      |        |PgDown|        |      |      |           |      |      |      | Prev | Next |      |        |
  * `------+------+--------+------+--------+-------------'           `-------------+------+------+------+------+--------'
- *   |    |      |        | Lclk | Rclk |                                       |VolUp |VolDn | Mute |      |      |
+ *   |    |      |        |      |      |                                       |VolUp |VolDn | Mute |      |      |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
  *                                 ,------|------|------|       |------+------+------.
- *                                 |      |      |      |       |      |      |Brwser|
- *                                 |      |      |------|       |------|      |Back  |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 | Lclk |      |------|       |------|      | Rclk |
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
 // NAVIGATION
 [NAV] = LAYOUT_ergodox(
-       RESET,   KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS,
+       RESET,   KC_TRNS, KC_TRNS,             KC_PGUP, KC_TRNS,             KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS, LGUI(LSFT(KC_LCBR)), KC_UP,   LGUI(LSFT(KC_RCBR)), KC_TRNS, KC_TRNS,
        KC_TRNS, KC_HOME, KC_LEFT,             KC_DOWN, KC_RIGHT,            KC_END,
-       KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_TRNS,             KC_BTN1, KC_BTN2,
+       KC_TRNS, KC_TRNS, KC_TRNS,             KC_PGDN, KC_TRNS,             KC_TRNS, KC_TRNS,
+       KC_TRNS, KC_TRNS, KC_TRNS,             KC_TRNS, KC_TRNS,
                                            KC_TRNS, KC_TRNS,
                                                     KC_TRNS,
-                                  KC_TRNS, KC_TRNS, KC_TRNS,
+                                  KC_BTN1, KC_TRNS, KC_TRNS,
     // right hand
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
        KC_TRNS,  KC_TRNS, KC_TRNS, KC_MS_U, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -147,7 +147,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_VOLU, KC_VOLD, KC_MUTE, KC_TRNS, KC_TRNS,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
-       KC_TRNS, KC_TRNS, KC_WBAK
+       KC_TRNS, KC_TRNS, KC_BTN2
 ),
 };
 
