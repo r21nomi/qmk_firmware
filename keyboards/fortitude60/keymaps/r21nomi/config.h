28690500a40dbd720e84c302c7ae948ef1322d2b
r21nomi <ryotakosu@gmail.com>
2018-08-26 14:52:06 +0900

    Fix led config for fortitude60

--- keyboards/fortitude60/keymaps/r21nomi/config.h
+++ keyboards/fortitude60/keymaps/r21nomi/config.h
@@ -31,11 +31,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define TAPPING_TERM 100
 
 #define USE_SERIAL_PD2
-/* #undef RGBLED_NUM */
-/* #define RGBLIGHT_ANIMATIONS */
-/* #define RGBLED_NUM 12 */
-/* #define RGBLIGHT_HUE_STEP 8 */
-/* #define RGBLIGHT_SAT_STEP 8 */
-/* #define RGBLIGHT_VAL_STEP 8 */
+#undef RGBLED_NUM
+#define RGBLIGHT_ANIMATIONS
+#define RGBLED_NUM 12
+#define RGBLIGHT_HUE_STEP 8
+#define RGBLIGHT_SAT_STEP 8
+#define RGBLIGHT_VAL_STEP 8
 
 #endif
